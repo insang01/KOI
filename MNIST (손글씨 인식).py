b1d04ef2adf65ab68c8e3ce23ed7ba45c933458e
insang01 <jeff00700@naver.com>
2018-05-29 16:58:30 +0900

    MNIST 작업
    -신경망 생성 완료
    -신경망 모델 학습 해야함

---
+++ MNIST (손글씨 인식).py
@@ -0,0 +1,65 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+# 머신러닝 학습의 Hello World 와 같은 MNIST(손글씨 숫자 인식) 문제를 신경망으로 풀어봅니다.
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("./mnist/data/", one_hot=True)
+
+#########
+# 옵션 설정
+######
+learning_rate = 0.001
+total_epoch = 30
+batch_size = 128
+
+# RNN 은 순서가 있는 자료를 다루므로,
+# 한 번에 입력받는 갯수와, 총 몇 단계로 이루어져있는 데이터를 받을지를 설정해야합니다.
+# 이를 위해 가로 픽셀수를 n_input 으로, 세로 픽셀수를 입력 단계인 n_step 으로 설정하였습니다.
+n_input = 28
+n_step = 28
+n_hidden = 128
+n_class = 10
+
+#########
+# 신경망 모델 구성
+######
+X = tf.placeholder(tf.float32, [None, n_step, n_input])
+Y = tf.placeholder(tf.float32, [None, n_class])
+
+W = tf.Variable(tf.random_normal([n_hidden, n_class]))
+b = tf.Variable(tf.random_normal([n_class]))
+
+# RNN 에 학습에 사용할 셀을 생성합니다
+# 다음 함수들을 사용하면 다른 구조의 셀로 간단하게 변경할 수 있습니다
+# BasicRNNCell,BasicLSTMCell,GRUCell
+cell = tf.nn.rnn_cell.BasicRNNCell(n_hidden)
+
+# RNN 신경망을 생성합니다
+# 원래는 다음과 같은 과정을 거쳐야 하지만
+# states = tf.zeros(batch_size)
+# for i in range(n_step):
+#     outputs, states = cell(X[[:, i]], states)
+# ...
+# 다음처럼 tf.nn.dynamic_rnn 함수를 사용하면
+# CNN 의 tf.nn.conv2d 함수처럼 간단하게 RNN 신경망을 만들어줍니다.
+# 겁나 매직!!
+outputs, states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
+
+# 결과를 Y의 다음 형식과 바꿔야 하기 때문에
+# Y : [batch_size, n_class]
+# outputs 의 형태를 이에 맞춰 변경해야합니다.
+# outputs : [batch_size, n_step, n_hidden]
+#        -> [n_step, batch_size, n_hidden]
+#        -> [batch_size, n_hidden]
+outputs = tf.transpose(outputs, [1, 0, 2])
+outputs = outputs[-1]
+model = tf.matmul(outputs, W) + b
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+
