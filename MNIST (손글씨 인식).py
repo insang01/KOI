a4a85b48c05b6f339a91fc6167b471883aa4c449
insang01 <39643555+insang01@users.noreply.github.com>
2018-06-02 00:46:13 +0900

    Update MNIST 손인식
    신경망 모델 학습함

--- MNIST (손글씨 인식).py
+++ MNIST (손글씨 인식).py
@@ -62,4 +62,41 @@ model = tf.matmul(outputs, W) + b
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=Y))
 optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+#############################++
+#########
+# 신경망 모델 학습
+######
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
 
+total_batch = int(mnist.train.num_examples/batch_size)
+
+for epoch in range(total_epoch):
+    total_cost = 0
+
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        # X 데이터를 RNN 입력 데이터에 맞게 [batch_size, n_step, n_input] 형태로 변환합니다.
+        batch_xs = batch_xs.reshape((batch_size, n_step, n_input))
+
+        _, cost_val = sess.run([optimizer, cost],
+                               feed_dict={X: batch_xs, Y: batch_ys})
+        total_cost += cost_val
+
+    print('Epoch:', '%04d' % (epoch + 1),
+          'Avg. cost =', '{:.3f}'.format(total_cost / total_batch))
+
+print('최적화 완료!')
+
+#########
+# 결과 확인
+######
+is_correct = tf.equal(tf.argmax(model, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+
+test_batch_size = len(mnist.test.images)
+test_xs = mnist.test.images.reshape(test_batch_size, n_step, n_input)
+test_ys = mnist.test.labels
+
+print('정확도:', sess.run(accuracy,
+                       feed_dict={X: test_xs, Y: test_ys}))
